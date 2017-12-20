6063cc890946dd776c81b011de35f51270d5c653
RITHIKSANJEEV97 <34641065+RITHIKSANJEEV97@users.noreply.github.com>
2017-12-21 00:06:34 +0530

    Add files via upload

---
+++ assignment/20-12-2017/swap.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int b,i,j;
+char a[200],temp;
+scanf("%s",a);
+printf("the string is %s \n",a);
+b=strlen(a);
+for(i=0;i<b-1;i++)
+{
+if(i%2==0)
+{
+temp=a[i];
+a[i]=a[i+1];
+a[i+1]=temp;
+}
+/*else
+{
+a[i]=a[i];
+}*/
+}
+printf("the ans is %s",a);
+}
